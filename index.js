225ed191ac9a6c325d038e0f4da4c89fdd4a3c53
Kevin95280 <coelhokev@ymail.com>
2025-10-18 17:27:25 +0200

    Remove dotenv configuration from index.js
    Removed dotenv import and configuration.

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 import express from 'express';
-import * as dotenv from 'dotenv';
 import router from './app/router.js';
-
-dotenv.config();
 
 const port = process.env.PORT || 3000;
 const app = express();
